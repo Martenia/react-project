3d09c0b6df6d57c651dfb34f1275a9cb2ea76618
Marta Nitek <marta.nitek@onet.pl>
2021-04-27 15:04:30 +0200

    Wykorzystanie Propsów

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
-import MyComponent from '../Hero/Hero.js';
+import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
   render() {
     return (
       <section className={styles.component}>
-        <MyComponent />
+        <Hero titleText={this.props.title} photoImg={this.props.img} />
+
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+
+        <div className={styles.columns}>
+          <Column titleCol={'Animals'} />
+          <Column titleCol={'Plants'} />
+          <Column titleCol={'Minerals'} />
+        </div>
+        
       </section>
     )
   }
