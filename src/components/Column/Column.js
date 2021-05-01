f873d2e60dcd5e20277ea2a107428404703e0217
Marta Nitek <marta.nitek@onet.pl>
2021-05-01 17:34:13 +0200

    Dodawanie Nowych Kolumn i Kart

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import {settings} from '../../data/dataStore.js';
 import Icon from '../Icon/Icon.js';
 
@@ -17,6 +17,7 @@ class Column extends React.Component {
     title: PropTypes.string,
     icon: PropTypes.node,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
   }
 
   /*   addCard(title){
@@ -34,7 +35,7 @@ class Column extends React.Component {
   } */
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
 
     return (
       <section className={styles.component}>
@@ -49,10 +50,9 @@ class Column extends React.Component {
           ))}
         </div>
         
-        {/* 
         <div>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div> */}
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
 
       </section>
     );
