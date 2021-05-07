f29b7e3d8dac6715eaa378bc5eadcf8596f68d47
Marta Nitek <marta.nitek@onet.pl>
2021-05-07 12:31:17 +0200

    Dodajemy Nawigację

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 // import App from './components/App/App';
-import App from './components/App/AppContainer';
+import App from './components/App/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
